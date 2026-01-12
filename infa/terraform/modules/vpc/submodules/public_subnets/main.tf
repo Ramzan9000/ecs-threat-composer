5bcfac446d88b822cbc8637d17aeb6d232333db8
Ramzan <ramzankhan7871@gmail.com>
2026-01-12 15:42:13 +0000

    test

--- infa/terraform/modules/vpc/submodules/public_subnets/main.tf
+++ infa/terraform/modules/vpc/submodules/public_subnets/main.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = var.vpc_id
   cidr_block              = var.public_subnets[count.index]
   map_public_ip_on_launch = true
-
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "${var.vpc_name}-public-${count.index}"
   }
