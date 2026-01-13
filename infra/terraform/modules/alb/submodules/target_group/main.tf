5477c472b08143eae591fd29bde48776ef9e25b0
Ramzan <ramzankhan7871@gmail.com>
2026-01-13 12:14:03 +0000

    updated workflows

--- infra/terraform/modules/alb/submodules/target_group/main.tf
+++ infra/terraform/modules/alb/submodules/target_group/main.tf
@@ -7,7 +7,7 @@ resource "aws_lb_target_group" "this" {
 
   health_check {
     interval            = 30
-    path                = "/"
+    path                = "/health"
     timeout             = 5
     healthy_threshold   = 3
     unhealthy_threshold = 3
